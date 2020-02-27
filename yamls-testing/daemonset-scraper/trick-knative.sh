5388e943cac507095b01e9d45cfaa41fda33e651
Tara Gu <guw@ibm.com>
2020-02-27 10:29:37 -0500

    testing

---
+++ yamls-testing/daemonset-scraper/trick-knative.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -xe
+set -o pipefail
+
+export INGRESSIP=$(kubectl get svc istio-ingressgateway -n istio-system -ocustom-columns=D:.status.loadBalancer.ingress[0].ip --no-headers)
+echo ">INGRESS: " $INGRESSIP
+
+export APP=$(kubectl get ksvc/echo-2 -ocustom-columns=D:.status.url --no-headers | cut -d'/' -f3)
+echo ">APP: " $APP
+
+echo "> hit the autoscaler with burst of requests"
+for i in `seq 50`; do
+    curl -sf -H "Host: $APP" "http://$INGRESSIP?sleep=10s"&
+done
+
+
+
+
